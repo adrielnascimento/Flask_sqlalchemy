38af9f4fd1b55b688484c9c0a53b6390e66cddc3
adrielnascimento <adrielnascimento62@gmail.com>
2024-04-07 13:20:58 -0300

    tabela
    criando a tabela

--- app/models/table.py
+++ app/models/table.py
@@ -1,2 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+# criação da table
+class User(db.Model): 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(100), nullable=False)
+    senha = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
