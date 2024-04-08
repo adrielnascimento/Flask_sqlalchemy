eb8b9fb6f5aad1a27ac716c9e2b3a290662136a1
Adriel <adrielnascimento62@gmail.com>
2024-04-08 18:55:23 -0300

    melhorando
    verificando metodo utilizado

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -16,10 +16,12 @@ def gera_response(status, nome_conteudo, conteudo, mensagem=False):
 # selecionar todos
 @user_blueprint.route("/usuarios", methods=['GET'])
 def selecionar_users(): 
-    users = User.query.all()
-    users_json = [c.to_json() for c in users]
-    return gera_response(200, "usuarios", users_json)
-
+    if request.method == 'GET': 
+        users = User.query.all()
+        users_json = [c.to_json() for c in users]
+        return gera_response(200, "usuarios", users_json)
+    else:
+        return 'erro! metodo invalido'
 # selecionar um
 @user_blueprint.route("/usuarios/<id>", methods=['GET'])
 def selecionar_user(id): 
@@ -30,34 +32,40 @@ def selecionar_user(id):
 # cadastrar 
 @user_blueprint.route("/usuarios/", methods=['POST'])
 def cadastrar(): 
-    # adicionar user
-    receber_user = request.json
-    cadastrar = User(nome=receber_user['nome'], senha=receber_user['senha'], email=receber_user['email'])
-    db.session.add(cadastrar)
-    db.session.commit()
-    return gera_response(204, "", {}, mensagem='cadastro realizado com sucesso!')
+    if request.method == 'POST': 
+        # adicionar user
+        receber_user = request.json
+        cadastrar = User(nome=receber_user['nome'], senha=receber_user['senha'], email=receber_user['email'])
+        db.session.add(cadastrar)
+        db.session.commit()
+        return gera_response(204, "", {}, mensagem='cadastro realizado com sucesso!')
+    else: 
+        return 'erro! metodo invalido'
 
 # atualizar
 @user_blueprint.route("/usuarios/<id>", methods=['PUT'])
 def update(id): 
-    # verificar se existe
-    verificar = User.query.get(id)
-    if verificar: 
-        # ver qual campo será alterado
-        receber_resp = request.json
-        if receber_resp:
-            if 'nome' in receber_resp: 
-                verificar.nome = receber_resp['nome']
-            if 'senha' in receber_resp: 
-                verificar.senha = receber_resp['senha']
-            if 'email' in receber_resp: 
-                verificar.email = receber_resp['email']
-            db.session.commit()
-            return gera_response(204, '', {}, mensagem='Usuário atualizado.')
+    if request.method == 'PUT': 
+        # verificar se existe
+        verificar = User.query.get(id)
+        if verificar: 
+            # ver qual campo será alterado
+            receber_resp = request.json
+            if receber_resp:
+                if 'nome' in receber_resp: 
+                    verificar.nome = receber_resp['nome']
+                if 'senha' in receber_resp: 
+                    verificar.senha = receber_resp['senha']
+                if 'email' in receber_resp: 
+                    verificar.email = receber_resp['email']
+                db.session.commit()
+                return gera_response(204, '', {}, mensagem='Usuário atualizado.')
+            else: 
+                return gera_response(400, '', {}, mensagem='Corpo da requisição vazio')
         else: 
-            return gera_response(400, '', {}, mensagem='Corpo da requisição vazio')
-    else: 
-        return gera_response(404, '', {}, mensagem='Usuario não encontrado!')
+            return gera_response(404, '', {}, mensagem='Usuario não encontrado!')
+    else:
+        return 'erro! metodo invalido'
 
 # deletar
 @user_blueprint.route("/usuarios/<id>", methods=['DELETE'])
